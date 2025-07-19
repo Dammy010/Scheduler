b9ea888be11193435a3b6b810f5a9eca2669545b
Damilare <da9783790@gmil.com>
2025-07-19 11:07:31 +0100

    update Scheduler api.js commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://scheduler-six-blue.vercel.app/api",
+  baseURL: "https://scheduler-six-blue.vercel.app",
   withCredentials: true, 
 });
 
